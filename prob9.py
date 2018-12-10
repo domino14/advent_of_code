ccce73cf2f9d0d356603db7d2b68eaf99b00cf60
Cesar Del Solar <delsolar@gmail.com>
2018-12-10 08:52:04 -0500

    fix prob 9, use linked list

--- prob9.py
+++ prob9.py
@@ -20,48 +20,49 @@ class Marble:
             pn = pn.previous
         return pn
 
+    def make_next(self, node):
+        self.next = node
+        node.previous = self
+
+    def print_layout(self):
+        start = self
+        next = start.next
+        values = [self.value]
+        while next.value != start.value:
+            values.append(next.value)
+            next = next.next_ct(1)
+        print(' '.join([str(v) for v in values]))
+
+
 scores = [0] * players
-
 initial_marble = Marble(0)
-initial_marble.next = initial_marble
-initial_marble.previous = initial_marble
-
-# marble_layout = [Marble(0)]
-
+initial_marble.make_next(initial_marble)
 current_marble_node = initial_marble
-
-
 
 
 def place_new_marble(marble, scores, cur_player):
     global current_marble_node
-    global marble_layout
     # print(f'Marble {marble}, cur_player {cur_player}')
     # otherwise, use existing marble
     if marble % 23 != 0:
         placeholder = current_marble_node.next_ct(2)
         placeholder_previous = current_marble_node.next_ct(1)
         new_node = Marble(marble)
-        placeholder_previous.next = new_node
-        new_node.previous = placeholder_previous
-        new_node.next = placeholder
-        placeholder.previous = new_node
+        placeholder_previous.make_next(new_node)
+        new_node.make_next(placeholder)
+
         current_marble_node = new_node
     else:
         # remove marble 7 spaces ccw
-        # to_remove = current_marble_idx - 7
-        
-        # if to_remove < 0:
-        #     to_remove = len(marble_layout) + to_remove
-        # marble_at = marble_layout[to_remove]
-        # Add marble we woulda placed, and the marble at the square
-        # scores[cur_player-1] += marble + marble_at
-        #print(f'removing marble {marble_at} (idx {to_remove})')
-        # del marble_layout[to_remove]
-        # current_marble_idx = to_remove
-        # print(f'layout meow: {marble_layout}, cur_marble_idx '
-        #       f'{current_marble_idx}')
-        current_marble_node
+        to_remove = current_marble_node.previous_ct(7)
+        scores[cur_player - 1] += marble + to_remove.value
+
+        to_remove_prev = current_marble_node.previous_ct(8)
+        to_remove_prev.make_next(to_remove.next)
+        current_marble_node = to_remove.next
+
+    # current_marble_node.print_layout()
+
 
 cur_player = 0
 for i in range(1, last_marble_pts+1):
@@ -70,5 +71,5 @@ for i in range(1, last_marble_pts+1):
         cur_player = 1
     place_new_marble(i, scores, cur_player)
     if i % 10000 == 0:
-        print(f'...{i} {len(marble_layout)}')
+        print(f'...{i}')
 print(f'ans {max(scores)}')

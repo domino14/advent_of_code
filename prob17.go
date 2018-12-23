e7aae4f958b5336d6c1d5b144a4a08c98ed7edda
Cesar Del Solar <delsolar@gmail.com>
2018-12-23 15:41:42 -0500

    ARGH I SOLVED THIS GODDAMN PROBLEM

---
+++ prob17.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const FountainX = 500
+
+var RecursedMap map[int]bool
+
+func parseRange(r string) (int, int) {
+	rs := strings.Split(r, "..")
+	rstart, _ := strconv.Atoi(rs[0])
+	rend, _ := strconv.Atoi(rs[1])
+	return rstart, rend
+}
+
+func addToMap(watertable map[int]map[int]rune, x int, y int, ch rune) {
+	if watertable[y] == nil {
+		watertable[y] = make(map[int]rune)
+	}
+	if ch == '~' && watertable[y][x] == '~' {
+		panic("Bad")
+	}
+	watertable[y][x] = ch
+}
+
+func createMap() (map[int]map[int]rune, int, int, int, int) {
+	cacheFilename := "/tmp/adventofcode_2018_17"
+	// cacheFilename = "./example17.txt"
+	dat, _ := ioutil.ReadFile(cacheFilename)
+	var minx, maxx, miny, maxy int
+	minx = 1000000
+	maxx = -1000000
+	miny = 1000000
+	maxy = -1000000
+
+	watertable := make(map[int]map[int]rune)
+
+	for _, line := range strings.Split(string(dat), "\n") {
+		var x, y, xstart, xend, ystart, yend int
+		coords := strings.Split(line, ", ")
+		vein := [][]string{}
+		for _, coord := range coords {
+			desc := strings.Split(coord, "=")
+			vein = append(vein, desc)
+		}
+		if vein[0][0] == "x" {
+			ystart, yend = parseRange(vein[1][1])
+			x, _ = strconv.Atoi(vein[0][1])
+			if x < minx {
+				minx = x
+			}
+			if x > maxx {
+				maxx = x
+			}
+			if ystart < miny {
+				miny = ystart
+			}
+			if yend > maxy {
+				maxy = yend
+			}
+			for y := ystart; y <= yend; y++ {
+				addToMap(watertable, x, y, '#')
+			}
+		}
+		if vein[0][0] == "y" {
+			xstart, xend = parseRange(vein[1][1])
+			y, _ = strconv.Atoi(vein[0][1])
+			if y < miny {
+				miny = y
+			}
+			if y > maxy {
+				maxy = y
+			}
+			if xstart < minx {
+				minx = xstart
+			}
+			if xend > maxx {
+				maxx = xend
+			}
+			for x := xstart; x <= xend; x++ {
+				addToMap(watertable, x, y, '#')
+			}
+		}
+	}
+	return watertable, minx, maxx, miny, maxy
+}
+
+func passable(wtable map[int]map[int]rune, x, y int) bool {
+	return wtable[y] == nil || wtable[y][x] == '|' || wtable[y][x] == 0
+}
+
+func moveSideways(wtable map[int]map[int]rune, miny, maxy, x, y, dir, dropsGen int) (int, int) {
+	seenMap := make(map[int]int)
+
+	for {
+		x += dir
+		idx := y*100000 + x
+
+		if passable(wtable, x, y) {
+			addToMap(wtable, x, y, '|')
+
+			if passable(wtable, x, y+1) {
+				if _, ok := RecursedMap[idx]; !ok {
+					traceDropPath(wtable, miny, maxy, x, y, dropsGen)
+				}
+				RecursedMap[idx] = true
+				return -1, -1
+			}
+
+		} else {
+			x -= dir
+			dir *= -1
+		}
+		seenMap[idx]++
+		if seenMap[idx] == 2 {
+			return x, y
+		}
+		if y > maxy {
+			break
+		}
+	}
+	return -1, -1
+}
+
+func traceDropPath(wtable map[int]map[int]rune, miny, maxy, x, y, dropsGen int) {
+	for y < maxy {
+		y = y + 1
+		if passable(wtable, x, y) {
+			// drop thru
+			addToMap(wtable, x, y, '|')
+		} else {
+			// Travel left and right and bounce back.
+			s1x, s1y := moveSideways(wtable, miny, maxy, x, y-1, -1, dropsGen)
+			s2x, s2y := moveSideways(wtable, miny, maxy, x, y-1, 1, dropsGen)
+
+			if s1x != -1 && s2x != -1 && s1y == s2y {
+				ysettle := s1y
+				var xsettle int
+				// sort s1x, s2x, FountainX
+				i := []int{s1x, s2x, FountainX}
+				sort.Ints(i)
+				if i[0] == FountainX {
+					xsettle = i[2]
+				} else {
+					xsettle = i[0]
+				}
+				addToMap(wtable, xsettle, ysettle, '~')
+			}
+			break
+		}
+		if dropsGen == 1281 {
+			// fmt.Println(countWater(wtable))
+			// fmt.Println(y)
+		}
+	}
+
+}
+
+func countWater(wtable map[int]map[int]rune, miny, maxy int) (int, int) {
+	var settled, sand int
+	for y, xs := range wtable {
+		if y < miny || y > maxy {
+			continue
+		}
+		for _, tileval := range xs {
+			if tileval == '~' {
+				settled++
+			}
+			if tileval == '|' {
+				sand++
+			}
+		}
+	}
+	return settled, sand
+}
+
+func main() {
+	wtable, minx, maxx, miny, maxy := createMap()
+	fmt.Printf("miny=%v, maxy=%v, minx=%v, maxx=%v, map=%v",
+		miny, maxy, minx, maxx, wtable)
+	lastSettled := -1
+	lastSand := -1
+	dropsGen := 0
+	RecursedMap = make(map[int]bool)
+	for {
+		RecursedMap = map[int]bool{}
+		fmt.Println("----------")
+		x, y := FountainX, 0
+		traceDropPath(wtable, miny, maxy, x, y, dropsGen)
+
+		settled, sand := countWater(wtable, miny, maxy)
+		if settled == lastSettled && sand == lastSand {
+			break
+		}
+		lastSettled, lastSand = settled, sand
+		dropsGen++
+		fmt.Printf("%v (%v, %v)\n", dropsGen, lastSettled, lastSand)
+	}
+	fmt.Printf("Part 1: %v (%v)", lastSettled+lastSand, dropsGen)
+}
--- prob17.py
+++ prob17.py
@@ -1,13 +1,18 @@
 from collections import defaultdict
 from get_data import get_data_lines
-
+from PIL import Image
+import uuid
 WATER_LOC = (500, 0)
 
+
+recursed = {}
 
 def create_map(lines):
     # map -- y, then x
     min_y = 1000000
+    min_x = 1000000
     max_y = -1000000
+    max_x = -1000000
     map = defaultdict(lambda: defaultdict(lambda: '.'))
     map[WATER_LOC[1]][WATER_LOC[0]] = '+'
 
@@ -26,6 +31,10 @@ def create_map(lines):
             if yend > max_y:
                 max_y = yend
             x = int(vein[0][1])
+            if x < min_x:
+                min_x = x
+            if x > max_x:
+                max_x = x
             for y in range(ystart, yend+1):
                 map[y][x] = '#'
 
@@ -37,16 +46,22 @@ def create_map(lines):
                 min_y = y
             if y > max_y:
                 max_y = y
+
+            if xstart < min_x:
+                min_x = xstart
+            if xend > max_x:
+                max_x = xend
+
             for x in range(xstart, xend+1):
                 map[y][x] = '#'
 
-    return map, min_y, max_y
+    return map, min_x, max_x, min_y, max_y
 
 
 def print_map(map, min_y, max_y):
     # These can be varied for debugging.
-    x_min = 420
-    x_max = 700
+    x_min = 475
+    x_max = 525
     strs = []
     for y in range(min_y, max_y+1):
         arr = []
@@ -57,7 +72,7 @@ def print_map(map, min_y, max_y):
     return '\n'.join(strs)
 
 
-def trace_drop_path(map, min_y, max_y, x, y):
+def trace_drop_path(map, min_y, max_y, x, y, drops_gen):
 
     # settled = False
     while y < max_y:
@@ -74,43 +89,33 @@ def trace_drop_path(map, min_y, max_y, x, y):
             # a point we have already seen TWICE (or goes off the edge).
             # Second subdrop goes right, bounces back, and stops when it hits
             # a point we hve already seen TWICE (or goes off the edge).
-            # IF the two points are in the same Y, pick the farthest from x = 500
+            # IF the two points are in the same Y, pick the farthest from
+            # x = 500
             #
             # print(f'Was {map[y][x]}, moving sideways on y={y-1}')
-            seen1 = move_sideways(map, min_y, max_y, x, y-1, -1)
-            seen2 = move_sideways(map, min_y, max_y, x, y-1, 1)
+            s1x, s1y = move_sideways(map, min_y, max_y, x, y-1, -1, drops_gen)
+            s2x, s2y = move_sideways(map, min_y, max_y, x, y-1, 1, drops_gen)
 
-            tpl1 = None
-            tpl2 = None
-            for tpl, times in seen1.items():
-                if times == 2:
-                    tpl1 = tpl
-                    break
-            for tpl, times in seen2.items():
-                if times == 2:
-                    tpl2 = tpl
-                    break
-
-            if tpl1 and tpl2 and tpl1[1] == tpl2[1]:
-                ysettle = tpl1[1]
-                if tpl1[0] < tpl2[0]:
-                    if tpl1[0] < WATER_LOC[0]:
-                        xsettle = tpl1[0]
-                    else:
-                        xsettle = tpl2[0]
+            # XXX: Note this is potentially an issue:
+            if s1x != -1 and s2x != -1 and s1y == s2y:
+                ysettle = s1y
+                locs = sorted([s1x, s2x, WATER_LOC[0]])
+                if locs[0] == WATER_LOC[0]:
+                    xsettle = locs[2]
+                elif locs[2] == WATER_LOC[0]:
+                    xsettle = locs[0]
                 else:
-                    if tpl2[0] < WATER_LOC[0]:
-                        xsettle = tpl2[0]
-                    else:
-                        xsettle = tpl1[0]
+                    xsettle = locs[0]
 
                 # print(f'Settling at {xsettle, ysettle}')
                 map[ysettle][xsettle] = '~'
-
             break
+        # if drops_gen == 1281 and y > 1900:
+        #     save_image(map, min_x, max_x, min_y, max_y,
+        #                f'drops_{drops_gen}_{uuid.uuid4()}.png')
 
 
-def move_sideways(map, min_y, max_y, x, y, dir):
+def move_sideways(map, min_y, max_y, x, y, dir, drops_gen):
     still_moving_sideways = True
     seen_map = defaultdict(int)
 
@@ -123,10 +128,14 @@ def move_sideways(map, min_y, max_y, x, y, dir):
             # print(f'Setting to |, cuz x={x} and y={y}')
             if map[y+1][x] in ('.', '|'):
                 # print(f'Tracing drop path again, since {x}, {y+1} was sand')
-                trace_drop_path(map, min_y, max_y, x, y)
+                if (x, y) not in recursed:
+                    trace_drop_path(map, min_y, max_y, x, y, drops_gen)
+                recursed[(x, y)] = True
                 still_moving_sideways = False
+                return -1, -1
         else:
             # It's a wall or drop,  bounce back.
+            # print(f'Bounce back bounce back bounce back')
             x -= dir
             dir *= -1
         # elif map[y][x] == '~':
@@ -136,9 +145,29 @@ def move_sideways(map, min_y, max_y, x, y, dir):
 
         seen_map[(x, y)] += 1
         if seen_map[(x, y)] == 2:
-            return seen_map
+            return x, y
 
-    return seen_map
+    return -1, -1
+
+
+def save_image(map, min_x, max_x, min_y, max_y, name):
+    colors = []
+    rgb_lookup = {
+        '+': [133, 255, 218],
+        '~': [62, 200, 242],
+        '|': [247, 210, 46],
+        '#': [135, 79, 20],
+        '.': [186, 170, 154],
+    }
+    for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            rgb = rgb_lookup[map[y][x]]
+            colors.extend(rgb)
+
+    colors = bytes(colors)
+    img = Image.frombytes('RGB', ((max_x - min_x + 1), (max_y - min_y + 1)),
+                          colors)
+    img.save(name)
 
 
 def count_water(map):
@@ -159,20 +188,21 @@ y=7, x=495..501
 x=501, y=3..7
 x=498, y=2..4
 x=506, y=1..2
-x=498, y=9..13
-x=503, y=10..13
-y=13, x=498..503""".split('\n')
+x=498, y=10..13
+x=504, y=10..13
+y=13, x=498..504""".split('\n')
 
-    # lines = get_data_lines(17)
+    lines = get_data_lines(17)
 
-    map, min_y, max_y = create_map(lines)
-    print(f'min_y={min_y}, max_y={max_y}')
+    map, min_x, max_x, min_y, max_y = create_map(lines)
+    print(f'min_y={min_y}, max_y={max_y}, min_x={min_x}, max_x={max_x}')
     last_settled_ct, last_sand_ct = None, None
     drops_gen = 0
     while True:
+        recursed = {}
         print('--------------')
         x, y = WATER_LOC
-        trace_drop_path(map, min_y, max_y, x, y)
+        trace_drop_path(map, min_y, max_y, x, y, drops_gen)
 
         settled_ct, sand_ct = count_water(map)
         if (settled_ct, sand_ct) == (last_settled_ct, last_sand_ct):
@@ -181,9 +211,10 @@ y=13, x=498..503""".split('\n')
         drops_gen += 1
         # if drops_gen % 10 == 0:
         print(f'{drops_gen} ({last_settled_ct, last_sand_ct})')
-        if drops_gen > 1275:
-            with open(f'map_{drops_gen}.txt', 'w') as f:
-                f.write(print_map(map, min_y, max_y))
-                print(f'Wrote to file map_{drops_gen}.txt')
-
+        # print(print_map(map, min_y, max_y))
+        if drops_gen % 1000 == 0:
+            save_image(map, min_x, max_x, min_y, max_y,
+                       f'drops_{drops_gen}.png')
+    save_image(map, min_x, max_x, min_y, max_y,
+               f'drops_{drops_gen}.png')
     print(f'Part 1: {last_settled_ct + last_sand_ct} (drops={drops_gen})')

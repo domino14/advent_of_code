32a4592b8d31301d5bc1e976cb5e0d0a44b50034
Cesar Del Solar <delsolar@gmail.com>
2018-12-25 01:38:55 -0500

    ugh

--- p21_decompiled.py
+++ p21_decompiled.py
@@ -1,37 +1,31 @@
-
-r0 = 12454098   # I control this.
-
 r1 = 0
-
-loops = 0
 
 part1 = False
 last_r1 = None
 
+seen_numbers = set()
+
 
 def inner_fn():
-    global r5, r1, loops, part1, last_r1
+    global r5, r1, part1, last_r1, seen_numbers
 
     while True:
-        loops += 1
         r1 = r1 + (r5 & 255)
         r1 = ((r1 & 16777215) * 65899) & 16777215
         if r5 < 256:
-            # THE STOPPING CONDITION
-            # if r1 == r0:
-            #     return False
-            # else:
-            #     return True
 
             if not part1:
                 print(f'Part 1: {r1}')
                 part1 = True
-            # return False
-            if r1 == 5970144 and last_r1 is not None:  # part 1 answer
-                print(f'Part 2: {last_r1}, cycle={r1}')
+            if r1 not in seen_numbers:
+                seen_numbers.add(r1)
+                last_r1 = r1
+                return True
+            else:
+                print(f'Part 2: {last_r1}')
+                # 1974943 is WRONG!
                 return False
-                # 1974943 is too low apparently.
-            last_r1 = r1
+
         else:
             r5 = int(r5 / 256)
 
@@ -43,6 +37,4 @@ while True:
     ret = inner_fn()
     if not ret:
         break
-
-
 

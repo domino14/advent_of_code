14ce5c5644fc7a21e6328bf1733a9aea90541012
César Del Solar <delsolar@gmail.com>
2021-12-02 00:19:50 -0500

    p2

--- 2021/2.py
+++ 2021/2.py
@@ -1,4 +1,33 @@
-input = open("/Users/cesar/02/input.txt", "r").read().strip()
+input = open("/Users/cesar/02/input.txt", "r").readlines()
 
 
-print("Result: {}".format(result))
+h = 0
+v = 0
+for line in input:
+    d, p = line.split()
+    if d == "forward":
+        v += int(p)
+    elif d == "down":
+        h += int(p)
+    elif d == "up":
+        h -= int(p)
+
+print(h * v)
+
+## two
+
+h = 0
+v = 0
+aim = 0
+
+for line in input:
+    d, p = line.split()
+    if d == "forward":
+        v += int(p)
+        h += aim * int(p)
+    elif d == "down":
+        aim += int(p)
+    elif d == "up":
+        aim -= int(p)
+
+print(h * v)
